d5e0bd8a979cb74bb1f2e9cf3c965eab10169d5a
STS-Engineer <administration.sts@avocarbon.com>
2026-01-22 10:37:51 +0100

    Update CORS origins for production deployment
    Removed local origins and added production origin for CORS.

--- app/main.py
+++ app/main.py
@@ -19,8 +19,7 @@ app = FastAPI()
 
 # ---- CORS ----
 origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "https://bt-renault.azurewebsites.net",
 ]
 app.add_middleware(
     CORSMiddleware,
